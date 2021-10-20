49bdb4c9864534e5284d133de6dd0638b98935c8
anhdt <anhdthe140963@fpt.edu.vn>
2021-10-20 21:37:54 +0700

    update: create class api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Role } from 'src/constant/role.enum';
+import { Roles } from 'src/decorator/roles.decorator';
+import { RolesGuard } from 'src/guards/roles.guard';
 import { AuthService } from './auth.service';
 import { LogInDto } from './dto/log-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
-
+import { readXlsxFile } from 'read-excel-file/node';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -14,5 +26,13 @@ export class AuthController {
   @Post('login')
   async signIn(@Body() logInDto: LogInDto): Promise<{ user; accessToken }> {
     return await this.authService.logIn(logInDto);
+  }
+  @UseGuards(AuthGuard(), RolesGuard)
+  @Roles(Role.Administrator)
+  @Post('gen-student-acc')
+  @UseInterceptors(FileInterceptor('file'))
+  async generateStudentAccount(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.path);
+    // const xlxs = await readXlsxFile(file, { sheet: 1 });
   }
 }
--- src/modules/classes/dto/create-classes.dto.ts
+++ src/modules/classes/dto/create-classes.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateClassDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @IsNotEmpty()
+  @IsNumber()
   teacherId: number;
 }
