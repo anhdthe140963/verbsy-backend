4a5d7376fc3eb87d9101e23efe75e2b4521e453c
anhdt <anhdthe140963@fpt.edu.vn>
2021-11-18 16:05:38 +0700

    update:get active games api

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -5,11 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameRepository } from './repositoty/game.repository';
 import { PassportModule } from '@nestjs/passport';
 import { ClassesRepository } from '../classes/repository/classes.repository';
+import { UserClassRepository } from '../user-class/repository/question.repository';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    TypeOrmModule.forFeature([GameRepository, ClassesRepository]),
+    TypeOrmModule.forFeature([
+      GameRepository,
+      ClassesRepository,
+      UserClassRepository,
+    ]),
   ],
   controllers: [GameController],
   providers: [GameService],
