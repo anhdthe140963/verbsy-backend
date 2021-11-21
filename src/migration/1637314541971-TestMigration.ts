894fb1da2e60e2f73eaab5eaeb1651ca6ac10c89
anhdt <anhdthe140963@fpt.edu.vn>
2021-11-21 19:47:37 +0700

    feat: migration using csv

--- src/migration/1637314541971-TestMigration.ts
+++ src/migration/1637314541971-TestMigration.ts
@@ -1,31 +1,40 @@
+import { Grade } from 'src/modules/grade/entities/grade.entity';
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { convertCsvToJson } from './convert/convert-csv-to-json';
 
 export class TestMigration1637314541971 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: 'test_migration',
-        columns: [
-          {
-            name: 'id',
-            type: 'int',
-            isPrimary: true,
-          },
-          {
-            name: 'name',
-            type: 'varchar',
-          },
-          {
-            name: 'created_at',
-            type: 'timestamp',
-            default: 'now()',
-          },
-        ],
-      }),
-      true,
-    );
+    // await queryRunner.createTable(
+    //   new Table({
+    //     name: 'test_migration',
+    //     columns: [
+    //       {
+    //         name: 'id',
+    //         type: 'int',
+    //         isPrimary: true,
+    //       },
+    //       {
+    //         name: 'name',
+    //         type: 'varchar',
+    //       },
+    //       {
+    //         name: 'created_at',
+    //         type: 'timestamp',
+    //         default: 'now()',
+    //       },
+    //     ],
+    //   }),
+    //   true,
+    // );
+    const json = await convertCsvToJson('src/migration/csv/grade.csv');
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into(Grade)
+      .values(json)
+      .execute();
   }
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('test_migration');
+    // await queryRunner.dropTable('test_migration');
   }
 }
