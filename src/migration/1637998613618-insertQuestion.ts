24efa67c752193987e4db073143222b9fa954c6c
Nguyen Manh Tri (K14 HL) <trinm1308@gmail.com>
2021-12-18 22:42:40 +0700

    fix: migration conflict

--- src/migration/1637998613618-insertQuestion.ts
+++ src/migration/1637998613618-insertQuestion.ts
@@ -15,17 +15,5 @@ export class insertQuestion1637998613618 implements MigrationInterface {
       .execute();
   }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        //         const questionJson = await convertCsvToJson('src/migration/csv/answer_question_csv/question/question_1_12.csv');
-        // await queryRunner.manager
-        //   .createQueryBuilder()
-        //   .insert()
-        //   .into(Question)
-        //   .values(questionJson)
-        //   .execute();
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
