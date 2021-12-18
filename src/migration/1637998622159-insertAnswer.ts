985c0526e18d56e6034da513641524aafbd7ad58
unknown <quangnvhe141337@fpt.edu.vn>
2021-12-18 22:17:17 +0700

    New migration

--- src/migration/1637998622159-insertAnswer.ts
+++ src/migration/1637998622159-insertAnswer.ts
@@ -4,10 +4,10 @@ import { convertCsvToJson } from "./convert/convert-csv-to-json";
 export class insertAnswer1637998622159 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const answerJson = await convertCsvToJson('src/migration/csv/answer_question_csv/answer/answer1_12.csv');
-        for(const j of answerJson){
-           await queryRunner.query(`insert into answer(content, is_correct,questionId) values('${j['content']}','${j['isCorrect']}','${j['questionId']}')`); 
-        }
+        // const answerJson = await convertCsvToJson('src/migration/csv/answer_question_csv/answer/answer1_12.csv');
+        // for(const j of answerJson){
+        //    await queryRunner.query(`insert into answer(content, is_correct,questionId) values('${j['content']}','${j['isCorrect']}','${j['questionId']}')`); 
+        // }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
